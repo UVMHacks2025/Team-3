5ba9ac75378a6c5d86fd3858f5bb3aeea1c8254a
lcirne <lcirne@uvm.edu>
2025-03-01 10:48:23 -0500

    created database file

--- data/UserDB.py
+++ data/UserDB.py
@@ -1,3 +1,25 @@
+import pandas as pd
+import sqlite3
 '''
 Databse Management file
 '''
+
+
+# Read in example date
+def setup(cls):
+
+
+# Initialize sqlite
+
+
+
+
+
+# Add item
+
+# Remove item
+
+# Check for expirations
+
+# Check for low quantity
+## Ask representative for quantity threshhold
