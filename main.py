996981db9d5e75e1dd53f1d490715d99cc745950
beau albritton <balbritt@uvm.edu>
2025-03-01 13:27:06 -0500

    linking db to homepage

--- main.py
+++ main.py
@@ -51,44 +51,70 @@ INVENTORY DASHBOARD
 """
 @app.route("/dashboard", methods = ['GET','POST'])
 def dashboard():
-    error = 0
-    if request.method == "POST":
-        quantity = request.form.get("new_quantity") #data verification
-        if quantity.isdigit():
-            #Update db with new quantity for inventory
-            pass
-        else:
-            #send message saying the db has not been updated
-            error = "Invalid Input. Database has not been update."
+    # error = 0
+    # if request.method == "POST":
+    #     quantity = request.form.get("new_quantity") #data verification
+    #     if quantity.isdigit():
+    #         #Update db with new quantity for inventory
+    #         pass
+    #     else:
+    #         #send message saying the db has not been updated
+    #         error = "Invalid Input. Database has not been update."
 
-    #make rows be all the rows of the db
-    rows = [["Mac and Cheese", "Kraft", 3, "Vegetarian", "3/1/2025", "3/8/2025", "Hannafords"] ]
-    return render_template("inventory.html", 
-                           page_title = "Inventory",
-                           rows = rows,
-                           error = error)
+    # #make rows be all the rows of the db
+    # rows = [["Mac and Cheese", "Kraft", 3, "Vegetarian", "3/1/2025", "3/8/2025", "Hannafords"] ]
+    
+    database = db.Database()
+    database.load_db()
+    string = database.cur.execute(f'SELECT * FROM Inventory').fetchall()
+    print(string)
+    return render_template("home.html",
+                            string =  string ,
+                            )
 
 """
 ADD ITEM
 """
 @app.route("/add", methods = ['GET','POST'])
 def add():
+    database = db.Database()
+    database.load_db()
+
     if request.method == "POST":
         name = request.form.get("name")
+        amount = request.form.get("amount")
         donor = request.form.get("donor")
+        category = request.form.get("category")
         dietary_restrictions = [request.form.get('kosher'), request.form.get('halal'),
                                 request.form.get('vegetarian'), request.form.get('vegan')]
+        kosher = False 
+        halal = False
+        vegetarian  = False
+        vegan = False
         allergens = [request.form.get('dairy'), request.form.get('eggs'), request.form.get('fish'),
                      request.form.get('shellfish'), request.form.get('tree_nuts'), request.form.get('peanuts'),
                      request.form.get('wheat'), request.form.get('soybeans'), request.form.get('sesame')]
+        
         tag_list = request.form.get('tags')
         tags = tag_list.split(',')
         for i in range(0, len(tags)):
             tags[i] = tags[i].strip()
 
+        for i in range(len(dietary_restrictions)):
+            if dietary_restrictions[i] == 'kosher':
+                kosher = True
+            elif dietary_restrictions[i] == 'vegetarian':
+                vegetarian = True
+            elif dietary_restrictions[i] == 'halal':
+                halal = True
+            elif dietary_restrictions[i] == 'vegan':
+                vegan = True
+
         date = request.form.get('date')
         requested = request.form.get('requested')
+        
 
+        database.addItem(name,None,amount,category,donor,vegetarian,kosher,vegan,halal)
         # TODO: Put into database
 
 
