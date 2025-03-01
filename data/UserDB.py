4799a19ef0305c8e7c89504945a8edc059028d87
lcirne <lcirne@uvm.edu>
2025-03-01 10:52:15 -0500

    Updated UserDB and added dummy_data.csv

--- data/UserDB.py
+++ data/UserDB.py
@@ -3,22 +3,31 @@ import sqlite3
 '''
 Databse Management file
 '''
+
+data = pd.DataFrame(pd.read_csv("dummy_data.csv"))
+print(data)
+
+
+
+# Read in example date
+#def setup(cls):
+
 class databse:
     # Read in example date
     def setup(cls):
 
 
-    # Initialize sqlite
+# Initialize sqlite
 
 
 
 
 
-    # Add item
+# Add item
 
-    # Remove item
+# Remove item
 
-    # Check for expirations
+# Check for expirations
 
-    # Check for low quantity
-    ## Ask representative for quantity threshhold
+# Check for low quantity
+## Ask representative for quantity threshhold
