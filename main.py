a6948b051b0ff10dd12e8d7f4931c2e6541d72a4
amandabarth <112357592+amandabarth@users.noreply.github.com>
2025-03-01 11:32:50 -0500

    Added inventory page flask route

--- main.py
+++ main.py
@@ -32,7 +32,20 @@ INVENTORY DASHBOARD
 """
 @app.route("/dashboard", methods = ['GET','POST'])
 def home():
-    return render_template("home.html")
+    if request.method == "POST":
+        quantity = request.form.get("new_quantity") #data verification
+        if quantity.isDigit():
+            #Update Database with new quantity for inventory
+            pass
+        else:
+            #send message saying the db has not been updated
+            error = "Invalid Input. Database has not been update."
+
+    rows = ["Mac and Cheese", "Kraft", "3", "Vegetarian", "3/1/2025", "3/8/2025", "Hannafords"] #update with database
+    return render_template("inventory.html", 
+                           page_title = "Inventory",
+                           rows = rows
+                           error = error)
 
 
 
