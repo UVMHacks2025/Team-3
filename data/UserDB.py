555d5cd57f8a2e599c054991c3e2ac18efdc3a7d
BeeMillie <bee.millie137@gmail.com>
2025-03-01 11:42:47 -0500

    updateQuantity()

--- data/UserDB.py
+++ data/UserDB.py
@@ -6,6 +6,7 @@ Databse Management file
 
 class databse:
     # Read in example date
+    
     def __init__(self, df):
         self.df = df
         df = pd.DataFrame(pd.read_csv("dummy_data.csv"))
@@ -23,8 +24,17 @@ class databse:
         cn.execute("""INSERT INTO RALLYCATS (name, brand, quantity, category, 
                    donor, vegetarian, kosher, vegan, hallal) VALUES ({n}, {br},
                    {amt}, {cat}, {don}, {veget}, {kosh}, {vega}, {hall})""")
+        cn.commit()
 
 # Remove item
+    def removeItem(cn, n):
+        cn.execute("""DELETE FROM RALLYCATS WHERE name = {n}""")
+        cn.commit()
+
+# Change quantity
+    def changeQuantity(cn, n, amt):
+        cn.execute("""UPDATE RALLYCATS SET quantity = {amt} WHERE name = {n}""")
+        cn.commit()
 
 # Check for expirations
 
