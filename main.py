0d5843537170e3511436230d4339b81fdfc145a5
beau albritton <balbritt@uvm.edu>
2025-03-01 10:30:02 -0500

    adding base html structure, flask setup

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-import flask
+from flask import Flask, render_template, request, url_for, redirect
+
+app = Flask(__name__,static_folder='static')
+
+
+"""
+HOME PAGE
+"""
+@app.route("/", methods = ['GET','POST'])
+def home():
+    return render_template("home.html")
+
+
+
+"""
+LOGIN
+"""
+@app.route("/login", methods = ['GET','POST'])
+def home():
+    return render_template("home.html")
+
+
+
+"""
+INVENTORY DASHBOARD
+"""
+@app.route("/dashboard", methods = ['GET','POST'])
+def home():
+    return render_template("home.html")
+
+
+
