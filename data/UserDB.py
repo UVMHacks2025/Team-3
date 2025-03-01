9db3e9b2eda40ab4fbb7c428eb5d1f4a80a3c487
beau albritton <balbritt@uvm.edu>
2025-03-01 10:52:05 -0500

    db

--- data/UserDB.py
+++ data/UserDB.py
@@ -3,23 +3,22 @@ import sqlite3
 '''
 Databse Management file
 '''
+class databse:
+    # Read in example date
+    def setup(cls):
 
 
-# Read in example date
-def setup(cls):
-
-
-# Initialize sqlite
-
-
+    # Initialize sqlite
 
 
 
-# Add item
 
-# Remove item
 
-# Check for expirations
+    # Add item
 
-# Check for low quantity
-## Ask representative for quantity threshhold
+    # Remove item
+
+    # Check for expirations
+
+    # Check for low quantity
+    ## Ask representative for quantity threshhold
